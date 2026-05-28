211fd3ee7203dcac2ae2e2a4903ce941861eee02
Viacheslav Dykyi <dykyi.viacheslav@gmail.com>
2026-05-28 16:01:16 +0300

    Fix of the bonus homework 6_1

--- bonus_lesson/bonus_homework_6_1.py
+++ bonus_lesson/bonus_homework_6_1.py
@@ -1,13 +1,11 @@
 # Порахувати кількість унікальних символів в строці. Якщо їх більше 10 - вивести в консоль True, інакше - False.
 # Строку отримати за допомогою функції input()
+from enum import unique
 
 new_string = input("Please enter a new string: ")
-counter = 0
-for i in new_string:
-    if new_string.count(i) == 1:
-        counter += 1
-print(f'Кількість унікальних символів в строці: {counter}')
-if counter > 10:
+unique_words = len (set(new_string.lower()))
+print(f'Кількість унікальних символів в строці: {unique_words}')
+if unique_words > 10:
     print(True)
 else:
     print(False)
