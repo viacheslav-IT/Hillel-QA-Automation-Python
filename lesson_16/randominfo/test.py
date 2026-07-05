47899806201c0938f6754fcf409a915c3240bd76
Viacheslav Dykyi <dykyi.viacheslav@gmail.com>
2026-07-05 16:35:27 +0300

    PR_14 comment is fixed (test.py and __init__.py files)

--- lesson_16/randominfo/test.py
+++ lesson_16/randominfo/test.py
@@ -1,7 +1,3 @@
 from randominfo import Person
-
-try:
-    person = Person()
-    print(person.full_name, person.gender, person.country, person.address)
-except IndexError as e:
-    print(f'Отримуємо наступну помилку: {e}')
+person = Person()
+print(person.full_name, person.gender, person.country, person.address)
